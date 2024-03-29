c704bb5dbad6c33be242cfb16fe1df09aa17ed78
Игорь <butovskii_77@mail.ru>
2024-03-29 17:07:02 +0300

    My first commit

--- homework.py
+++ homework.py
@@ -1,69 +1,78 @@
-class InfoMessage:
-    """Информационное сообщение о тренировке."""
-    pass
+import datetime as dt
 
 
-class Training:
-    """Базовый класс тренировки."""
+class Calculator:
+    def __init__(self, limit):
+        self.records = []
+        self.limit = limit
 
-    def __init__(self,
-                 action: int,
-                 duration: float,
-                 weight: float,
-                 ) -> None:
-        pass
+    def add_record(self, record):
+        self.records.append(record)
 
-    def get_distance(self) -> float:
-        """Получить дистанцию в км."""
-        pass
+    def get_today_stats(self):
+        today = dt.date.today()
+        today_stats = sum(record.amount for record in self.records
+                          if record.date == today)
+        return today_stats
 
-    def get_mean_speed(self) -> float:
-        """Получить среднюю скорость движения."""
-        pass
+    def get_week_stats(self):
+        today = dt.date.today()
+        week_ago = today - dt.timedelta(7)
+        week_stats = sum(record.amount for record in self.records
+                         if week_ago <= record.date <= today)
+        return week_stats
 
-    def get_spent_calories(self) -> float:
-        """Получить количество затраченных калорий."""
-        pass
-
-    def show_training_info(self) -> InfoMessage:
-        """Вернуть информационное сообщение о выполненной тренировке."""
-        pass
+    def remained(self):
+        return self.limit - self.get_today_stats()
 
 
-class Running(Training):
-    """Тренировка: бег."""
-    pass
+class Record:
+    def __init__(self, amount, comment, date=None):
+        self.amount = amount
+        self.comment = comment
+        if date is not None:
+            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+        else:
+            self.date = dt.date.today()
 
 
-class SportsWalking(Training):
-    """Тренировка: спортивная ходьба."""
-    pass
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        calories_remained = self.remained()
+        if calories_remained <= 0:
+            return 'Хватит есть!'
+        return ('Сегодня можно съесть что-нибудь ещё, но с общей '
+                f'калорийностью не более {calories_remained} кКал')
 
 
-class Swimming(Training):
-    """Тренировка: плавание."""
-    pass
+class CashCalculator(Calculator):
+    USD_RATE = 60.0
+    EURO_RATE = 70.0
+    RUB_RATE = 1.0
+
+    def get_today_cash_remained(self, currency):
+        cash_remained = self.remained()
+        if cash_remained == 0:
+            return 'Денег нет, держись'
+        currencies = {
+            'eur': ('Euro', self.EURO_RATE),
+            'usd': ('USD', self.USD_RATE),
+            'rub': ('руб', self.RUB_RATE),
+        }
+        if currency not in currencies:
+            return 'No such currency.'
+        sign, rate = currencies.get(currency)
+        cash_remained = round(cash_remained / rate, 2)
+        if cash_remained > 0:
+            return f'На сегодня осталось {cash_remained} {sign}'
+        cash_remained = abs(cash_remained)
+        return f'Денег нет, держись: твой долг - {cash_remained} {sign}'
 
 
-def read_package(workout_type: str, data: list) -> Training:
-    """Прочитать данные полученные от датчиков."""
-    pass
+cash_calculator = CashCalculator(1000)
 
+cash_calculator.add_record(Record(amount=145, comment='кофе'))
+cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+cash_calculator.add_record(Record(amount=3000, comment='бар в Танин др', date='08.11.2019'))
 
-def main(training: Training) -> None:
-    """Главная функция."""
-    pass
-
-
-if __name__ == '__main__':
-    packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
-    ]
-
-    for workout_type, data in packages:
-        training = read_package(workout_type, data)
-        main(training)
-
-
+print(cash_calculator.get_today_cash_remained('rub'))
